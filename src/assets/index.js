258db675125509f08bb8deca56bbcfedce3f2f0c
unknown <khanfahadbangash998877@gmail.com>
2023-10-02 05:37:14 -0700

    client react

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import logoDark from './logoDark.png';
+import logoLight from './logoLight.png';
+import cratImg from './cart.png';
+import paymentLogo from './paymentLogo.png';
+import googleLogo from './googleLogo.png';
+import githubLogo from './githubLogo.png';
+
+export {logoDark,logoLight,cratImg,paymentLogo,googleLogo,githubLogo}
